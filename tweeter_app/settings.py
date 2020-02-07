0e4bab0e74a8d566b342d7ada1ba97f033bc8e31
walter-scott <mail.wscott@gmail.com>
2020-02-07 12:23:22 -0600

    adding django heroku import

--- tweeter_app/settings.py
+++ tweeter_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku # <- this line goes near the top of the file
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users', # new
-    'tweets', # new
+    'tweets', # newpython manage.py runserver
 ]
 
 AUTH_USER_MODEL = 'users.CustomUser' # new
